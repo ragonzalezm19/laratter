7ce7afa51ceb49f7290d0678eb1bb1e6928d9095
Roberto Gonzalez <ragonzalezm.19@gmail.com>
2018-03-24 18:32:53 -0300

    Clase 5:
     - Usando @yield, @extends y @section

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,99 +1,21 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Laravel</title>
+@section('content')
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+<div class="title m-b-md">
+    Laratter, by <a href="https://platzi.com"> Platzi </a>
+</div>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+@if(isset($teacher))
+<p>Profesor: {{ $teacher }}</p>
+@else
+<p>Profesor a definir</p>
+@endif
 
-            .full-height {
-                height: 100vh;
-            }
+<div class="links">
+    @foreach($links as $link => $text)
+    <a href="{{ $link }}">{{ $text }}</a>
+    @endforeach
+</div>
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laratter by Platzi
-                </div>
-
-                @if(isset($teacher))
-                <p>Profesor: {{ $teacher }}</p>
-                @else
-                <p>Profesor a definir</p>
-                @endif
-
-                <div class="links">
-                    @foreach($links as $link => $text)
-                    <a href="{{ $link }}">{{ $text }}</a>
-                    @endforeach
-                </div>
-            </div>
-        </div>
-    </body>
-</html>
+@endsection
