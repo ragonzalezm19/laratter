9dce47f3b3118211615864fa94099e8c9d1dcb92
Roberto Gonzalez <ragonzalezm.19@gmail.com>
2018-03-24 18:11:27 -0300

    Clase 3:
    - Trabajando con if
    - Trabajando con foreach

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,15 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $links = [
+        'https://platzi.com/laravel' => 'Curso de Laravel',
+        'https://laravel.com' => 'Página de Laravel'
+    ];
+
+    return view('welcome', [
+        // 'teacher' => 'Guido Contreras Woda',
+        'links' => $links,
+    ]);
 });
 
 Route::get('/acerca', function () {
