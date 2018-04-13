293f730df445890f9b0c93de73c2e82986583ca8
Roberto Gonzalez <ragonzalezm.19@gmail.com>
2018-04-13 18:26:19 -0300

    Traer un solo mensaje de la Base de Datos

---
+++ app/Http/Controllers/MessagesController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Message;
+use Illuminate\Http\Request;
+
+class MessagesController extends Controller
+{
+    //
+    public function show(Message $message)
+    {
+        // $message = Message::find($id);
+
+        return view('messages.show', [
+            'message' => $message,
+        ]);
+    }
+}
