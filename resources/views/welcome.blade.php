6eb195627d385c2c2d33b9d6d565936e5226cfc4
Roberto Gonzalez <ragonzalezm.19@gmail.com>
2018-04-13 18:10:34 -0300

    Usando Eloquent el ORM de Laravel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,10 +12,10 @@
 <div class="row">
     @forelse ($messages as $message)
         <div class="col-6">
-        <img src="{{ $message['image'] }}" alt="" class="img-thumbnail">
+        <img src="{{ $message->image }}" alt="" class="img-thumbnail">
         <p class="card-text">
-            {{ $message['content']}}
-            <a href="/messages/{{$message['id']}}">Leer más</a>
+            {{ $message->content}}
+            <a href="/messages/{{$message->id}}">Leer más</a>
         </p>
         </div>
     @empty
