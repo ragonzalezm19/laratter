0867813ac0c8540364b2b85e56371059072449e5
Roberto Gonzalez <ragonzalezm.19@gmail.com>
2018-04-17 17:17:08 -0300

    Validacion y creacion de datos en Laravel con Eloquent

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Message;
 use Illuminate\Http\Request;
+use App\Http\Requests\CreateMessageRequest;
 
 class MessagesController extends Controller
 {
@@ -17,8 +18,13 @@ class MessagesController extends Controller
         ]);
     }
 
-    public function create(Request $request)
+    public function create(CreateMessageRequest $request)
     {
-       dd($request->all());
+       $message = Message::create([
+           'content' => $request->input('message'),
+           'image' => 'http://lorempixel.com/600/338?' . mt_rand(0, 1000)
+       ]);
+
+       return redirect("/messages/$message->id");
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,9 +11,14 @@
 </div>
 <div class="row">
     <form action="/messages/create" method="post">
-        {{ csrf_field() }}
         <div class="form-group">
-            <input type="text" name="message" class="form-control" placeholder="Qué estas pensando?">
+            {{ csrf_field() }}
+            <input type="text" name="message" class="form-control @if($errors->has('message')) is-invalid @endif" placeholder="Qué estas pensando?">
+            @if ($errors->has('message'))
+                @foreach ($errors->get('message') as $error)
+                    <div class="invalid-feedback"> {{ $error }} </div>
+                @endforeach
+            @endif
         </div>
     </form>
 </div>
